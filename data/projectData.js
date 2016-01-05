d2acab4d147a87417b4a04745b4d7e12c8cbde89
liujy <liujy@itdayang.com>
2016-01-05 21:15:07 +0800

    add all folder

---
+++ data/projectData.js
@@ -0,0 +1,574 @@
+{
+    "data":
+    [
+
+        {
+            "title": "问题1",
+            "description": "问题描述",
+            "issueId": "#001",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Closed"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题2",
+            "description": "问题描述",
+            "issueId": "#002",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题3",
+            "description": "问题描述",
+            "issueId": "#003",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题4",
+            "description": "问题描述",
+            "issueId": "#004",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题5",
+            "description": "问题描述",
+            "issueId": "#005",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题6",
+            "description": "问题描述",
+            "issueId": "#006",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题7",
+            "description": "问题描述",
+            "issueId": "#007",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题8",
+            "description": "问题描述",
+            "issueId": "#008",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题9",
+            "description": "问题描述",
+            "issueId": "#009",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题10",
+            "description": "问题描述",
+            "issueId": "#010",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题11",
+            "description": "问题描述",
+            "issueId": "#011",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题12",
+            "description": "问题描述",
+            "issueId": "#012",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题13",
+            "description": "问题描述",
+            "issueId": "#013",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题14",
+            "description": "问题描述",
+            "issueId": "#014",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题15",
+            "description": "问题描述",
+            "issueId": "#015",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题16",
+            "description": "问题描述",
+            "issueId": "#016",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题16",
+            "description": "问题描述",
+            "issueId": "#016",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题17",
+            "description": "问题描述",
+            "issueId": "#017",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题18",
+            "description": "问题描述",
+            "issueId": "#018",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题19",
+            "description": "问题描述",
+            "issueId": "#019",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        },
+        {
+            "title": "问题20",
+            "description": "问题描述",
+            "issueId": "#020",
+            "createTime": 1451989731964,
+            "author": "xx一号",
+            "assignee": "由谁来解决",
+            "statue": "Open"
+            "updatedTime": 1451989825924,
+            "discussList":[
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz二号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                },
+                {
+                    "userName":"zz一号",
+                    "text": "你做了啥",
+                    "time": 1451989825924
+                }   
+            ]
+        }
+
+    ]
+}
