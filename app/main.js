612ed04e6213b1bd96164249303da5eeb254e821
guyl <guyl@itdayang.com>
2016-01-10 11:12:09 +0800

    add main.js

--- app/main.js
+++ app/main.js
@@ -5,11 +5,19 @@ angular.module("app",["ngRoute", "listModule", "detailModule"])
             templateUrl: "view/list.html",
             controller: "listController"
         })
-        .when("/detail/:dno", {
+        .when("/detail", {
             templateUrl: "view/detail.html",
             controller: "detailController"
         })
+        .when("/newIssue", {
+            templateUrl: "view/newIssue.html",
+            controller: "newIssueController"
+        })
+        .when("/editIssue", {
+            templateUrl: "view/editIssue.html",
+            controller: "editIssueController"
+        })
         .otherwise({
-            redirectTo: "/list/open"
+            redirectTo: "/detail"
         })
 }])
