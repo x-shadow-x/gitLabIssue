cbf503e25ffb888f32aaa59c6b82bebee3f84035
guyl <guyl@itdayang.com>
2016-01-10 11:11:17 +0800

    add editIssuePage.js

---
+++ app/editIssuePage.js
@@ -0,0 +1,5 @@
+angular.module("editIssueModule", []);
+app.controller('editIssueController', ['$scope','$location',"$routeParams", "$http","$timeout", function($scope, $location, $routeParams, $http,$timeout){
+
+
+}]);
