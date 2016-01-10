275e8bfad6fb874f02037e1bd8e030535b686203
guyl <guyl@itdayang.com>
2016-01-10 17:20:32 +0800

    add newIssuePage.js

--- app/newIssuePage.js
+++ app/newIssuePage.js
@@ -1,3 +1,26 @@
-angular.module("newIssueModule", []).controller("newIssueController",function($scope) {
+var app=angular.module("newIssueModule", []);
+app.controller("newIssueController",function($scope) {
+    $scope.data={};
+    $scope.getData = function(cb) {
+        $.getJSON("data/projectData.js", function(result) {
+            console.log(result.data,"=============================|||||||||||||||||============================");
+            $scope.data = result.data;
+            cb();
+            $scope.$digest();
+        });
+/*        $http.get("data/projectData.js").then(function(data) {
+            console.log(data.data.data.pageSize);
+            $scope.data = data.data.data;
+            cb();
+        },function(err) {
+            console.log(err);
+        });*/
+    };
+/*    $scope.getData();*/
+    $scope.submit=function(){
 
+    };
+    $scope.cancel=function(){
+
+    };
 });
