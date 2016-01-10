49fc1e552053de73fbcf5533d9e127068d793ec4
guyl <guyl@itdayang.com>
2016-01-10 17:20:10 +0800

    add editIssuePage.js

--- app/editIssuePage.js
+++ app/editIssuePage.js
@@ -1,5 +1,21 @@
-angular.module("editIssueModule", []);
+var app=angular.module("editIssueModule", []);
 app.controller('editIssueController', ['$scope','$location',"$routeParams", "$http","$timeout", function($scope, $location, $routeParams, $http,$timeout){
-
+    $scope.data={};
+    $scope.getData = function(cb) {
+        $.getJSON("data/projectData.js", function(result) {
+            console.log(result.data,"=============================|||||||||||||||||============================");
+            $scope.data = result.data;
+            cb();
+            $scope.$digest();
+        });
+/*        $http.get("data/projectData.js").then(function(data) {
+            console.log(data.data.data.pageSize);
+            $scope.data = data.data.data;
+            cb();
+        },function(err) {
+            console.log(err);
+        });*/
+    };
+    
 
 }]);
