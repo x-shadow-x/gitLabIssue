b65a20ebd77cc381cee2783d9ee91a81666c0fa2
guyl <guyl@itdayang.com>
2016-01-11 13:04:28 +0800

    add editIssuePage.js

--- app/editIssuePage.js
+++ app/editIssuePage.js
@@ -1,7 +1,11 @@
-var app=angular.module("editIssueModule", []);
+//var app=angular.module("editIssueModule", []);
 app.controller('editIssueController', ['$scope','$location',"$routeParams", "$http","$timeout", function($scope, $location, $routeParams, $http,$timeout){
     $scope.data={};
+    $scope.assigneeList = []; 
+    $scope.versionList = []; 
+    setTimeout(function(){console.log($scope.assigneeList)},2000);
     $scope.getData = function(cb) {
+        console.log('edit');
         $.getJSON("data/projectData.js", function(result) {
             console.log(result.data,"=============================|||||||||||||||||============================");
             $scope.data = result.data;
@@ -16,6 +20,35 @@ app.controller('editIssueController', ['$scope','$location',"$routeParams", "$ht
             console.log(err);
         });*/
     };
-    
+    $scope.getData(function(){
+        $scope.getassignee();
+        $scope.getversion();
+    });
+    $scope.getassignee=function(){
+        var n={};
+        var l=$scope.data.length;
+        for(var i=0;i<l;i++){
+            var assignee=$scope.data[i].assignee;
+            if(!n[assignee]){
+                n[assignee]=true;
+                $scope.assigneeList.push(assignee);
+            }
+        }
+    }
+    $scope.getversion=function(){
+        var n={};
+        var l=$scope.data.length;
+        for(var i=0;i<l;i++){
+            var version=$scope.data[i].version;
+            if(!n[version]){
+                n[version]=true;
+                $scope.versionList.push(version);
+            }
+        }
+    }
+    $scope.submit=function(){
+        var valT=$scope.title;
+     
+    }
 
 }]);
